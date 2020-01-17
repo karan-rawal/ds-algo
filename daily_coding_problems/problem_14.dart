ec9f999f13ee0b10a82b5e5724965c108bc95ac6
Karan Rawal <kr14nov@outlook.com>
2020-01-17 18:28:35 +0530

    Finding the area of a circle.

---
+++ daily_coding_problems/problem_14.dart
@@ -0,0 +1,10 @@
+/**
+ * Medium - Google
+ * 
+ * The area of a circle is defined as pi * r^2.
+ * Estimate the value of pi upto 3 decimal places using a
+ * monte carlo method.
+ * 
+ * Hint: The basic equation of a circle is x2 + y2 = r2
+ * Read Proper Explanation: https://academo.org/demos/estimating-pi-monte-carlo/
+ */
